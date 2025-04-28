a09d1ba830b8ba770cefecc49b603e210302a869
Kajur <kajurahenrysp@gmail.com>
2025-04-28 14:52:52 +0300

    APP routing and sign in

---
+++ Components/MobileSidebar.tsx
@@ -0,0 +1,40 @@
+import { SidebarComponent } from "@syncfusion/ej2-react-navigations";
+import React from "react";
+import { Link } from "react-router";
+import NavItems from "./NavItems";
+
+const MobileSidebar = () => {
+  let sidebar: SidebarComponent;
+  const toggleSideBar = () => {
+    sidebar.toggle();
+  };
+  return (
+    <div className="mobile-sidebar wrapper">
+      <header>
+        <Link to="/">
+          <img
+            src="/assets/icons/logo.svg"
+            alt="logo"
+            className="size-[30px]"
+          />
+          <h2>Tour agency</h2>
+        </Link>
+        <button onClick={toggleSideBar}>
+          <img src="/assets/icons/menu.svg" alt="menu" className="size-7" />
+        </button>
+      </header>
+      <SidebarComponent
+        width={270}
+        ref={(Sidebar) => (sidebar = Sidebar)}
+        created={() => sidebar.hide()}
+        closeOnDocumentClick={true}
+        showBackdrop={true}
+        type="over"
+      >
+        <NavItems handleClick={toggleSideBar}/>
+      </SidebarComponent>
+    </div>
+  );
+};
+
+export default MobileSidebar;
---
+++ app/routes/Admin/Users.tsx
@@ -0,0 +1,13 @@
+import { Header } from "Components";
+import React from "react";
+
+const Users = () => {
+  return (
+    <main className="dashboard wrapper">
+      <Header title="Users" description="Current users" />
+      Users
+    </main>
+  );
+};
+
+export default Users;
